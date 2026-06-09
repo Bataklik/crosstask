8fc79a7fe4e77739af9b3cf3fa97df68f15621ae
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-09 16:21:39 +0200

    feat: :sparkles: add TaskHeader component

---
+++ app/tasks/TaskHeader.tsx
@@ -0,0 +1,7 @@
+export function TaskHeader() {
+    return (
+        <div className="flex items-center justify-center">
+            <h1 className="text-2xl font-bold">CrossTasks</h1>
+        </div>
+    );
+}
