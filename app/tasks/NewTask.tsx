c7c958ab931e607eac7a8b459741dd8df5cd5ea2
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-09 17:03:40 +0200

    feat: :sparkles: update NewTask component with props for task management

--- app/tasks/NewTask.tsx
+++ app/tasks/NewTask.tsx
@@ -1,7 +1,26 @@
-export function NewTask() {
+import { useEffect, useState } from "react";
+
+interface NewTaskProps {
+    task: string | null;
+    setTask: React.Dispatch<React.SetStateAction<string | null>>;
+    addTask: (task: string | null) => void;
+}
+export function NewTask({ task, setTask, addTask }: NewTaskProps) {
     return (
-        <div className="bg-green-500">
-            <p>New Task</p>
+        <div className="flex px-4 items-center justify-center my-6">
+            <input
+                type="text"
+                placeholder="Add new Task"
+                className="flex-3 border w-full border-purple-700 sm:px-4 sm:py-3 md:px-2 md:py-2 rounded-md"
+                onChange={(e) => setTask(e.target.value)}
+            />
+
+            <button
+                className="flex-1 bg-purple-700 text-white sm:py-3 md:px-4 md:py-2 rounded-md ml-2 hover:bg-purple-600 hover:cursor-pointer"
+                onClick={() => addTask(task)}
+            >
+                Add new Task
+            </button>
         </div>
     );
 }
