a03adc05fe306d02a99128c19fee985a32255d58
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-10 10:26:46 +0200

    feat: :sparkles: refactor TaskList component to use separate TableCaption, TableItem, and TableBody functions

--- app/tasks/TaskList.tsx
+++ app/tasks/TaskList.tsx
@@ -5,49 +5,63 @@ interface TasksListProps {
     setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
 }
 
+function TableCaption({ title }: { title?: string }) {
+    return (
+        <caption className="caption-top font-medium text-lg">
+            {title ? title : "List of Tasks"}
+        </caption>
+    );
+}
+
+function TableItem({ task }: { task: Task }) {
+    return (
+        <tr className="border-b-2 border-purple-700">
+            <td className="justify-center py-8 text-center ">
+                <p>{task.id}.</p>
+            </td>
+            <td className="justify-center py-8 text-center">
+                <p className="text-wrap">{task.title}</p>
+            </td>
+            <td className="justify-center py-8 text-center">
+                <input
+                    className="w-4 h-4"
+                    type="checkbox"
+                    checked={task.completed}
+                />
+            </td>
+        </tr>
+    );
+}
+
+function TableBody() {
+    return (
+        <tbody>
+            <TableItem
+                task={{
+                    id: 1,
+                    title: "Cleaning the bathroom",
+                    completed: false,
+                }}
+            />
+
+            <TableItem
+                task={{
+                    id: 2,
+                    title: "Cleaning the hall",
+                    completed: false,
+                }}
+            />
+        </tbody>
+    );
+}
+
 export function TaskList({ tasks, setTasks }: TasksListProps) {
     return (
         <div className="flex mx-4 justify-center ">
             <table className="w-full ">
-                <caption className="caption-top font-medium text-lg">
-                    List of Tasks
-                </caption>
-                <thead className="border-solid border-b-2 border-purple-700">
-                    <tr className="underline">
-                        <th className="py-2">Task id.</th>
-                        <th className="py-2">Task title.</th>
-                        <th className="py-2">Compeleted?</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="border-b-2 border-purple-700">
-                        <td className="justify-center py-8 text-center ">
-                            <p>1.</p>
-                        </td>
-                        <td className="justify-center py-8 text-center">
-                            <p className="text-wrap">
-                                Cleaning the bathroom Cleaning the bathroom
-                            </p>
-                        </td>
-                        <td className="justify-center py-8 text-center">
-                            <input className="w-4 h-4" type="checkbox" />
-                        </td>
-                    </tr>
-
-                    <tr className="">
-                        <td className="justify-center py-8 text-center ">
-                            <p>2.</p>
-                        </td>
-                        <td className="justify-center py-8 text-center">
-                            <p className="text-wrap">
-                                Cleaning the hall Cleaning the hall
-                            </p>
-                        </td>
-                        <td className=" justify-center py-8 text-center">
-                            <input className="w-4 h-4" type="checkbox" />
-                        </td>
-                    </tr>
-                </tbody>
+                <TableCaption />
+                <TableHead />
+                <TableBody />
             </table>
         </div>
     );
