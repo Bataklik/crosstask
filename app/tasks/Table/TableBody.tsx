458dab9c0b92a067aa17acadf5511d65a1080b06
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-10 16:13:13 +0200

    feat: :white_check_mark: enhance TableBody component to accept toggleTask prop for task management

--- app/tasks/Table/TableBody.tsx
+++ app/tasks/Table/TableBody.tsx
@@ -1,11 +1,16 @@
 import type { Task } from "~/types";
 import { TableItem } from "./TableItem";
 
-export function TableBody({ tasks }: { tasks: Task[] }) {
+interface TableBodyProps {
+    tasks: Task[];
+    toggleTask: (taskId: number) => void;
+}
+
+export function TableBody({ tasks, toggleTask }: TableBodyProps) {
     return (
         <tbody>
             {tasks.map((task) => (
-                <TableItem task={task} />
+                <TableItem key={task.id} task={task} toggleTask={toggleTask} />
             ))}
         </tbody>
     );
