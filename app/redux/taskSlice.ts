a5352ecfc5087e2add16ce9f4765ac0a21bb4f58
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-11 22:00:42 +0200

    feat: :heavy_plus_sign: add task slice with initial state and reducers

---
+++ app/redux/taskSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { Task } from "~/types";
+
+export interface TaskState {
+    tasks: Task[];
+}
+
+const initialState: TaskState = {
+    tasks: [],
+};
+
+export const taskSlice = createSlice({
+    name: "tasks",
+    initialState,
+    reducers: {
+        addTask: (state, action: PayloadAction<Task>) => {},
+        removeTask: (state, action: PayloadAction<number>) => {},
+        toggleTask: (state, action: PayloadAction<number>) => {},
+    },
+});
