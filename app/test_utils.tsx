f0f5a12afe5343ab6cfcc015b1714321d0804f1b
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-13 14:41:32 +0200

    feat: :heavy_plus_sign: enhance testing utilities with custom render function and extended options for Redux integration

--- app/test_utils.tsx
+++ app/test_utils.tsx
@@ -1,6 +1,10 @@
-import React, { type ReactElement } from "react";
+import React, { type PropsWithChildren, type ReactElement } from "react";
 import { render, type RenderOptions } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { Provider } from "react-redux";
 
+import type { AppStore, RootState, PreloadedState } from "./redux/store";
+import { setupStore } from "./redux/store";
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
     return <body>{children}</body>;
 };
@@ -9,6 +13,39 @@ const customRender = (
     ui: ReactElement,
     options?: Omit<RenderOptions, "wrapper">,
 ) => render(ui, { wrapper: AllTheProviders, ...options });
+
+// This type interface extends the default options for render from RTL, as well
+// as allows the user to specify other things such as preloadedState, store.
+interface ExtendedRenderOptions extends Omit<
+    RenderOptions,
+    "queries" | "wrapper"
+> {
+    preloadedState?: PreloadedState;
+    store?: AppStore;
+}
+
+export function renderWithProviders(
+    ui: React.ReactElement,
+    extendedRenderOptions: ExtendedRenderOptions = {},
+) {
+    const {
+        preloadedState = {},
+        // Automatically create a store instance if no store was passed in
+        store = setupStore(preloadedState),
+        ...renderOptions
+    } = extendedRenderOptions;
+
+    const Wrapper = ({ children }: PropsWithChildren) => (
+        <Provider store={store}>{children}</Provider>
+    );
+
+    // Return an object with the store, user, and all of RTL's query functions
+    return {
+        store,
+        user: userEvent.setup(),
+        ...render(ui, { wrapper: Wrapper, ...renderOptions }),
+    };
+}
 
 export * from "@testing-library/react";
 export { customRender as render };
