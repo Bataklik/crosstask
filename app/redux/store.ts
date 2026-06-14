a7291240be2beb10b1018ddfeba12bd91a5fa2b8
Bataklik <burak.mikail.aktas@outlook.com>
2026-06-14 15:55:10 +0200

    feat: :heavy_plus_sign: implement redux-persist for state management with local storage

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,19 +1,93 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import taskReducer from "./taskSlice";
 
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+const createNoopStorage = () => {
+    return {
+        getItem(_key: any) {
+            return Promise.resolve(null);
+        },
+        setItem(_key: any, value: any) {
+            return Promise.resolve(value);
+        },
+        removeItem(_key: any) {
+            return Promise.resolve();
+        },
+    };
+};
+const createBrowserStorage = () => {
+    return {
+        getItem(key: string) {
+            return Promise.resolve(window.localStorage.getItem(key));
+        },
+        setItem(key: string, value: string) {
+            window.localStorage.setItem(key, value);
+            return Promise.resolve();
+        },
+        removeItem(key: string) {
+            window.localStorage.removeItem(key);
+            return Promise.resolve();
+        },
+    };
+};
+
 const rootReducer = combineReducers({
     tasks: taskReducer,
 });
 
-export function setupStore(preloadedState?: PreloadedState) {
+//! Redux Persist Configuration
+const storage =
+    typeof window !== "undefined"
+        ? createBrowserStorage()
+        : createNoopStorage();
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["tasks"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+function setupStore(preloadedState?: Partial<RootState>, isTest = false) {
     return configureStore({
-        reducer: rootReducer,
+        reducer: (isTest
+            ? rootReducer
+            : persistedReducer) as typeof rootReducer,
         preloadedState,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({
+                serializableCheck: {
+                    // Ignore redux-persist actions
+                    ignoredActions: [
+                        FLUSH,
+                        REHYDRATE,
+                        PAUSE,
+                        PERSIST,
+                        PURGE,
+                        REGISTER,
+                    ],
+                },
+            }),
     });
 }
+
+export const store = setupStore();
+export const persistor = persistStore(store);
 
 export type PreloadedState = Parameters<typeof rootReducer>[0];
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = AppStore["dispatch"];
-export const store = setupStore();
